c2d34bd353a485bb3feb2bede74c5974c45fc694
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-12-11 22:20:12 +0100

    New Ui - Comparer View sketch
    -- Finished sketch for new comparer view.

--- src/main/java/pl/magzik/_new/controller/ComparerController.java
+++ src/main/java/pl/magzik/_new/controller/ComparerController.java
@@ -1,15 +1,40 @@
 package pl.magzik._new.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComparerController extends PanelController {
 
     @FXML
     private TextField pathTextField;
+
+    @FXML
+    private ListView<File> originalListView;
+
+    @FXML
+    private ListView<File> duplicateListView;
+
+    public void initialize() {
+        List<File> files = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            files.add(new File("txt.txt"));
+        }
+        ObservableList<File> list = FXCollections.observableList(files);
+
+        originalListView.setItems(list);
+
+
+
+        duplicateListView.setItems(FXCollections.observableList(List.of(new File("dup.txt"))));
+    }
 
     @FXML
     private void handleChoosePath() {
